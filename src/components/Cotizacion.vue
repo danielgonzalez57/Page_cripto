4c7160146523a6157f7fbfa434e778476b20e99c
Daniel Gonzalez <94912412+danielgonzalez57@users.noreply.github.com>
2024-04-20 16:33:06 -0400

    primer commit

---
+++ src/components/Cotizacion.vue
@@ -0,0 +1,23 @@
+<script setup>
+    
+</script>
+<template>
+    <div v-if="mostrarResultado" class="contenedor-resultado">
+        <h2>
+          Cotizacion
+        </h2>
+
+        <div class="resultado">
+          <img 
+            :src="'https://cryptocompare.com/' + cotizacion.IMAGEURL" 
+            alt="imagen">
+          <div>
+            <p>El precio es de: <span>{{ cotizacion.PRICE }}</span></p>
+            <p>Precio mas alto es: <span>{{ cotizacion.HIGHDAY }}</span></p>
+            <p>Precio mas bajo del dia: <span>{{ cotizacion.LOWDAY }}</span></p>
+            <p>Variacion ultimas 24hr: <span>{{ cotizacion.CHANGEPCT24HOUR }}%</span></p>
+            <p>Ultima actualizacion: <span>{{ cotizacion.LASTUPDATE }}</span></p>
+          </div>
+        </div>
+      </div>
+</template>
